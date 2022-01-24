04778967d20ec982d762ea8e23e3228b9c7c0634
ritul_mahapatra <ritul.mahapatra@gmail.com>
2022-01-24 13:32:22 +0530

    new changes

--- controller/sheetOperations.js
+++ controller/sheetOperations.js
@@ -1,11 +1,11 @@
 import { googleSheetsInstance } from "../model/getGoogleSheetsInstance.js";
-import {auth} from '../model/auth.js'
+// import {auth} from '../model/auth.js'
 
 const spreadsheetId = "1VPp_VqFgJAKbjiKlgiG_KR77jQbZXwjmt-uBwsEkN1U";
 
 const insertData = async()=>{
     await googleSheetsInstance.spreadsheets.values.append({
-        auth, //auth object
+        auth:process.env.GOOGLE_API_KEY, //auth object
         spreadsheetId, //spreadsheet id
         range: "Sheet1!A:B", //sheet name and range of cells
         valueInputOption: "USER_ENTERED", // The information will be passed according to what the usere passes in as date, number or text
@@ -17,7 +17,7 @@ const insertData = async()=>{
 
 const readData = async(sheetName, columns) =>{
     const readData = await googleSheetsInstance.spreadsheets.values.get({
-        auth, //auth object
+        auth: process.env.GOOGLE_API_KEY, //auth object
         spreadsheetId, // spreadsheet id
         range: `${sheetName}!${columns}`, //range of cells to read from.
     })
--- model/getGoogleSheetsInstance.js
+++ model/getGoogleSheetsInstance.js
@@ -1,8 +1,7 @@
-import { auth } from "./auth.js";
 import {google} from 'googleapis';
+import dotenv from 'dotenv';
 
-const authClientObject = await auth.getClient();
-
-const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+dotenv.config();
+const googleSheetsInstance = google.sheets({ version: "v4", auth: process.env.GOOGLE_API_KEY });
 
 export {googleSheetsInstance}
